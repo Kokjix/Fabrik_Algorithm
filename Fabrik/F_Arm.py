de64c5c248407788bc0347fc88175a1d9b41b4a5
Baran Berk Bağcı <53092278+Kokjix@users.noreply.github.com>
2021-02-10 13:20:01 +0300

    Fabrik Algorithm

---
+++ Fabrik/F_Arm.py
@@ -0,0 +1,200 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import math 
+
+global TOLERANCE
+
+TOLERANCE = 0.1
+
+class F_ARM(object):
+
+    def __init__(self):
+        self.joint_angles = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+        self.delta_thetas = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+        self.link_lenghts = [26.5, 28.1831, 20.0]
+
+
+
+class JOY(object):
+
+    def __init__(self):
+        self.axes = [10.0, 10.0, 10.0, 10.0, 10.0, 10.0]
+        self.buttons = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+    
+    def set_axes(self, new_axis_0, new_axis_1, new_axis_2, new_axis_3, new_axis_4, new_axis_5):
+        self.axes[0] = new_axis_0
+        self.axes[1] = new_axis_1
+        self.axes[2] = new_axis_2
+        self.axes[3] = new_axis_3
+        self.axes[4] = new_axis_4
+        self.axes[5] = new_axis_5
+
+    def get_axes(self,axes_index):
+        return self.axes[axes_index]
+
+    def set_buttons(self, new_button_0, new_button_1, new_button_2, new_button_3, new_button_4,  new_button_5,  new_button_6,  new_button_7,  new_button_8,  new_button_9,  new_button_10,  new_button_11):
+        self.buttons[0] = new_buttons_0
+        self.buttons[1] = new_buttons_1
+        self.buttons[2] = new_buttons_2
+        self.buttons[3] = new_buttons_3
+        self.buttons[4] = new_buttons_4
+        self.buttons[5] = new_buttons_5
+        self.buttons[6] = new_button_6
+        self.buttons[7] = new_buttons_7
+        self.buttons[8] = new_buttons_8
+        self.buttons[9] = new_buttons_9
+        self.buttons[10] = new_buttons_10
+        self.buttons[11] = new_buttons_11
+
+    def get_buttons(self, buttons_index):
+        return self.buttons[buttons_index]
+
+
+
+
+
+class COORDINATS(object):
+    def __init__(self, X, Y, Z):
+        self.coordinats = [X, Y, Z]
+
+    def get_x(self):
+        return self.coordinats[0]
+
+    def get_y(self):
+        return self.coordinats[1]
+    
+    def get_z(self):
+        return self.coordinats[2]
+
+
+def calculate_distance(point1, point2):
+    a = math.pow(point1[0] - point2[0], 2) + math.pow(point1[1] - point2[1], 2) + math.pow(point1[2] - point2[2], 2)
+
+    b = math.sqrt(a)
+    return b
+
+
+
+
+def dot_product(point1, point2):
+    
+
+    result = 0.0
+
+    result += point1[0] * point2[0]
+    result += point1[1] * point2[1]   #Tek satırda yazmayı dene 
+    result += point1[2] * point2[2]
+
+    return result
+
+def angle_of_vectors(point1, point2):
+    
+    base_point = [0,0,0]
+    
+    lenght_of_1 = calculate_distance(point1, base_point)
+    lenght_of_2 = calculate_distance(point2, base_point)
+
+    angle = (math.acos(dot_product(point1,point2)/(lenght_of_1 * lenght_of_2))*180)/math.pi
+
+    return angle
+       
+
+def rotate_on_xy(to_be_rotated, angle):
+    radian_angle = angle * math.pi/180
+
+    new_x = to_be_rotated[0] * math.cos(radian_angle) - to_be_rotated[1] * math.sin(radian_angle)
+    new_y = to_be_rotated[0] * math.sin(radian_angle) + to_be_rotated[1] * math.cos(radian_angle)       
+
+
+    to_be_rotated[0] = new_x
+    to_be_rotated[1] = new_y
+
+    return to_be_rotated
+
+def crd_multipication(point, factor):
+    cord = []
+    new_x = factor * point[0]
+    new_y = factor * point[1]
+    new_z = factor * point[2]
+
+    cord.append(new_x)
+    cord.append(new_y)
+    cord.append(new_z)
+    
+    return cord
+
+def cosinus_theorem(center, one, two):
+    a = calculate_distance(center, one)
+    b = calculate_distance(center, two)
+    c = calculate_distance(one, two)
+
+    my_angle = (math.acos((a*a + b*b - c*c)/(2*a*b))*180)/math.pi
+
+    return my_angle
+
+
+def find_angle(center, before, nex, joint_1_angle):
+    my_angle = cosinus_theorem(center, before, nex)
+
+    new_before = [before[0] - center[0], before[1] - center[1], before[2] - center[2]]
+    new_next = [nex[0] - center[0], nex[1] - center[1], nex[2] - center[2]]
+
+    cross_product_y = (new_before[2] * new_next[0]) - (new_before[0] * new_next[2])
+
+    if ((joint_1_angle < 90) and (joint_1_angle > 90)) and cross_product_y > 0:
+        my_angle = 360 - my_angle
+
+    if ((joint_1_angle < 90) or (joint_1_angle > 90)) and cross_product_y > 0:
+        my_angle = 360 - my_angle
+
+    return my_angle
+
+
+
+
+
+def FABRIK_algorithm(my_joints, link_lenghts, new_end_point_pos, REACH):
+    global TOLERANCE
+    i = 0
+    r = 0
+    lamb = 0
+    distance_from_begining = abs(calculate_distance(new_end_point_pos, my_joints[0]))
+
+    if distance_from_begining > REACH:
+        for i in range(len(my_joints)):
+            r = abs(calculate_distance(new_end_point_pos, my_joints[i]))
+
+            lamb = link_lenghts[i]/r
+            my_joints[i + 1] = crd_multipication(my_joints[i], 1-lamb) + crd_multipication(new_end_point_pos, lamb)
+    else:
+        b = my_joints[0]
+        distance_to_target = calculate_distance(new_end_point_pos, my_joints[len(my_joints) - 1])
+        
+        while distance_to_target > TOLERANCE:
+            my_joints[len(my_joints) - 1] = new_end_point_pos
+
+            for i in range(len(my_joints) - 2, -1, -1):
+                r = abs(calculate_distance(my_joints[i + 1], my_joints[i]))
+                lamb = link_lenghts[i] / r
+
+                my_joints[i][0] = crd_multipication(my_joints[i+1], 1 - lamb)[0] + crd_multipication(my_joints[i], lamb)[0]
+                my_joints[i][1] = crd_multipication(my_joints[i+1], 1 - lamb)[1] + crd_multipication(my_joints[i], lamb)[1]
+                my_joints[i][2] = crd_multipication(my_joints[i+1], 1 - lamb)[2] + crd_multipication(my_joints[i], lamb)[2]
+
+            my_joints[0] = b
+
+            for i in range(len(my_joints) - 1):
+                r = abs(calculate_distance(my_joints[i + 1], my_joints[i]))
+                lamb = link_lenghts[i]/ r
+
+                my_joints[i + 1][0] = crd_multipication(my_joints[i], 1 - lamb)[0] + crd_multipication(my_joints[i+1], lamb)[0]
+                my_joints[i + 1][1] = crd_multipication(my_joints[i], 1 - lamb)[1] + crd_multipication(my_joints[i+1], lamb)[1]
+                my_joints[i + 1][2] = crd_multipication(my_joints[i], 1 - lamb)[2] + crd_multipication(my_joints[i+1], lamb)[2]
+
+            distance_to_target = abs(calculate_distance(my_joints[len(my_joints) - 1], new_end_point_pos))
+
+
+
+
---
+++ Fabrik/fabrik_v1.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import rospy
+from std_msgs.msg import Float64 as F64
+from sensor_msgs.msg import Joy
+from F_Arm import *
+from std_msgs.msg import String
+from coordinate import *
+import math
+
+new_massage = False
+
+max_reach = 74.6831
+
+joy_msg = JOY()
+
+
+global X, Y, Z
+
+X = 0
+Y = 0
+Z = 0
+
+
+def joy_callback(msg):
+    #new_massage = True
+    global X, Y, Z
+    X += msg.axes[3] * 0.10
+    Y += msg.axes[0] * 0.10
+    Z += msg.axes[1] * 0.10
+    
+
+    
+
+
+if __name__ == "__main__":
+    my_joints = []
+    my_joints.append([0.0,0.0,7.0])
+    my_joints.append([0.0,0.0,33.5])
+    my_joints.append([27.3,0.0,26.5])
+    my_joints.append([47.3,0.0,26.5])
+    #arm = F_ARM()
+
+    link_lengths = [26.5, 28.1831, 20]
+    REACH = 74.6831
+
+    
+
+    rospy.init_node('fabrik')
+    joint_1_publisher = rospy.Publisher('/joint__1_controller/command', F64, queue_size=10)
+    joint_2_publisher = rospy.Publisher('/joint__2_controller/command', F64, queue_size=10)
+    joint_3_publisher = rospy.Publisher('/joint__3_controller/command', F64, queue_size=10)
+    joint_4_publisher = rospy.Publisher('/joint__4_controller/command', F64, queue_size=10)
+    joint_5_publisher = rospy.Publisher('/joint__5_controller/command', F64, queue_size=10)
+    joint_6_publisher = rospy.Publisher('/joint__6_controller/command', F64, queue_size=10)
+    #right_finger_publisher = rospy.Publisher('/rover_arm_right_finger/command', F64, queue_size=10)
+    #left_finger_publisher = rospy.Publisher('/rover_arm_left_finger/command', F64, queue_size=10)
+    
+    rospy.Subscriber('joy', Joy, joy_callback)
+
+    rate = rospy.Rate(150)
+
+    count = 0
+    first_run = True
+
+    while not rospy.is_shutdown():
+
+        
+        if first_run:
+            X = 0
+            Y = 0
+            Z = 0
+            joint_1_angles = 0.0
+            first_run = False
+
+       
+        new_end_point_pos = [47.3, 0.0, 26.5]
+        new_end_point_pos[0] = new_end_point_pos[0] + X
+        new_end_point_pos[1] = new_end_point_pos[1] + Y
+        new_end_point_pos[2] = new_end_point_pos[2] + Z
+
+        rospy.loginfo_throttle(2,"Target X = %s Y = %s Z= %s" %(new_end_point_pos[0], new_end_point_pos[1], new_end_point_pos[2]))
+
+        joint_1_angle_differance = (-1.0) * joint_1_angles
+
+        referance_coord_point = [4,0,0]
+        proj_joint_1 = [new_end_point_pos[0], new_end_point_pos[1], 0.0]       
+        joint_1_angles = angle_of_vectors(proj_joint_1, referance_coord_point)
+
+        if proj_joint_1[1] < 0:
+            joint_1_angles = joint_1_angles * (-1)
+        rospy.loginfo_throttle(2,"Joint_1 angle: %s" %joint_1_angles)
+        
+        joint_1_angle_differance += joint_1_angles
+
+        rotate_on_xy(my_joints[0], joint_1_angle_differance)
+        rotate_on_xy(my_joints[1], joint_1_angle_differance)
+        rotate_on_xy(my_joints[2], joint_1_angle_differance)
+        rotate_on_xy(my_joints[3], joint_1_angle_differance)
+
+        FABRIK_algorithm(my_joints, link_lengths, new_end_point_pos, REACH)
+
+        
+        joint_5_angle = find_angle(my_joints[2], my_joints[1], my_joints[3], joint_1_angles)
+        joint_3_angle = find_angle(my_joints[1], my_joints[0], my_joints[2], joint_1_angles)
+        joint_2_angle = find_angle(my_joints[0], [0.0,0.0,0.0], my_joints[1], joint_1_angles)
+
+        joint_angles = [joint_1_angles, 180.0 - joint_2_angle, 90.0 - (joint_3_angle + 14.38138), 0.0, (joint_5_angle + 75.61862) - 270.0]
+
+        joint1_last = (joint_angles[0]*math.pi)/180
+        joint2_last = (joint_angles[1]*math.pi)/180
+        joint3_last = (joint_angles[2]*math.pi)/180
+        joint4_last = (joint_angles[3]*math.pi)/180
+        joint5_last = (joint_angles[4]*math.pi)/180
+        joint6_last = 0.0
+        
+        joint_1_publisher.publish(joint1_last)
+        joint_2_publisher.publish(joint2_last)
+        joint_3_publisher.publish(joint3_last)
+        joint_4_publisher.publish(joint4_last)
+        joint_5_publisher.publish(joint5_last)
+        joint_6_publisher.publish(joint6_last)
+
+        rate.sleep()
+        
+    
+
+
+
+
